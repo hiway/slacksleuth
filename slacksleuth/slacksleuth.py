865b5ea6660acf8f2ae38f7bd82768e16ea698ab
Harshad Sharma <harshad@sharma.io>
2018-08-21 12:56:45 +0530

    Add a sprinkling of docstrings

--- slacksleuth/slacksleuth.py
+++ slacksleuth/slacksleuth.py
@@ -10,6 +10,17 @@ from plumbum.cmd import imagesnap  # brew install imagesnap
 
 class SlackSleuth(object):
     def __init__(self, token='', channel='', config_file='~/.slacksleuth'):
+        """
+        SlackSleuth monitors active applications.
+        It triggers when "Slack" app is the active app,
+        takes a photo using built-in camera on MacBooks,
+        posts the photo to the configured channel on slack,
+        and finally locks the screen to prevent further (ab)use.
+
+        :param token: Slack App OAuth2 token.
+        :param channel: Slack channel name to post.
+        :param config_file: Path to config file to store credentials.
+        """
         self.token = token
         self.channel = channel
         self.config_file = os.path.expanduser(config_file)
@@ -22,14 +33,23 @@ class SlackSleuth(object):
         self.client = SlackClient(self.token)
 
     def active_app(self):
+        """
+        :return: Name of active application.
+        """
         return NSWorkspace.sharedWorkspace() \
             .activeApplication()['NSApplicationName']
 
     def setup(self):
+        """
+        Configure credentials for Slack.
+        """
         self.token = click.prompt('Slack API Token:')
         self.channel = click.prompt('Slack Channel:')
 
     def save_config(self):
+        """
+        Save current configuration to file.
+        """
         with open(self.config_file, 'w') as config_file:
             json.dump({
                 'token': self.token,
@@ -37,6 +57,9 @@ class SlackSleuth(object):
             }, config_file)
 
     def load_config(self):
+        """
+        Load configuration from file.
+        """
         with open(self.config_file, 'r') as config_file:
             config = json.load(config_file)
             self.token = config.get('token', '')
@@ -45,6 +68,11 @@ class SlackSleuth(object):
             assert self.channel, 'Slack channel is not set.'
 
     def arm(self):
+        """
+        Arms the trap, starts watching active applications.
+
+        Once armed, user may stop the trap with Ctrl+C before switching to Slack.
+        """
         click.echo('Ready')
         while True:
             try:
@@ -63,9 +91,11 @@ class SlackSleuth(object):
         click.echo('Disarmed.')
 
     def trap(self, filename):
+        """Takes photo with build in camera."""
         imagesnap['-w', '0.5', filename]()
 
     def expose(self, filename):
+        """Sends photo to slack channel."""
         with open(filename, 'rb') as file_content:
             self.client.api_call(
                 "files.upload",
@@ -75,5 +105,6 @@ class SlackSleuth(object):
             )
 
     def demoralize(self):
+        """Locks screen."""
         subprocess.call(["/System/Library/CoreServices/Menu Extras/"
                          "User.menu/Contents/Resources/CGSession", "-suspend"])
